6fa98f0494524f4c3cbc5d72299381da6aea2563
ajibukunoluwa <ibukunajmoti@gmail.com>
2024-04-06 18:08:31 +0100

    implementation of 'granting access'

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from shopping_list.models import ShoppingItem, ShoppingList
 
+from typing import List
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
+    
 class ShoppingItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingItem
@@ -9,11 +17,24 @@ class ShoppingItemSerializer(serializers.ModelSerializer):
         
     def create(self, validated_data, **kwargs):
         validated_data['shopping_list_id'] = self.context['request'].parser_context['kwargs']['pk']
+        
+        if self.itemAlreadyExists(validated_data):
+            raise serializers.ValidationError("There's already this item on the list")
+        
         return super(ShoppingItemSerializer, self).create(validated_data)
+    
+    def itemAlreadyExists(self, validated_data: List):
+        return ShoppingList.objects.get(
+            id=validated_data['shopping_list_id']
+        ).shopping_items.filter(name=validated_data["name"], purchased=False)
         
 class ShoppingListSerializer(serializers.ModelSerializer):
-    shopping_items = ShoppingItemSerializer(many=True, read_only=True)
+    unpurchased_items = serializers.SerializerMethodField()
+    members = UserSerializer(many=True, read_only=True)
     
     class Meta:
         model = ShoppingList
-        fields = ['id', 'name', 'shopping_items']
+        fields = ["id", "name", "unpurchased_items", "members"]
+        
+    def get_unpurchased_items(self, obj):
+        return [{"name": shopping_item.name} for shopping_item in obj.shopping_items.filter(purchased=False)][:3]
--- shopping_list/views.py
+++ shopping_list/views.py
@@ -1,20 +1,45 @@
 from rest_framework import generics
+from shopping_list.api.permissions import (
+    AllShoppingItemsShoppingListMembersOnly,
+    ShoppingItemShoppingListMembersOnly,
+    ShoppingListMembersOnly,
+)
 from shopping_list.api.serializers import ShoppingListSerializer, ShoppingItemSerializer
 from shopping_list.models import ShoppingList, ShoppingItem
 
 class ListAddShoppingList(generics.ListCreateAPIView):
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
+    
+    def perform_create(self, serializer):
+        return serializer.save(members=[self.request.user])
+    
+    def get_queryset(self):
+        return ShoppingList.objects.filter(members=self.request.user)
 
 class ShoppingListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
     
-class AddShoppingItem(generics.CreateAPIView):
-    queryset = ShoppingItem.objects.all()
-    serializer_class = ShoppingItemSerializer
+    permission_classes = [ShoppingListMembersOnly]
+    
+# class AddShoppingItem(generics.CreateAPIView):
+#     queryset = ShoppingItem.objects.all()
+#     serializer_class = ShoppingItemSerializer
+    
+#     permission_classes = [AllShoppingItemsShoppingListMembersOnly]
     
 class ShoppingItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ShoppingItem.objects.all()
     serializer_class = ShoppingItemSerializer
     lookup_url_kwarg = "item_pk"
+    
+    permission_classes = [ShoppingItemShoppingListMembersOnly]
+    
+class ListAddShoppingItem(generics.ListCreateAPIView):
+    serializer_class = ShoppingItemSerializer
+    permission_classes = [AllShoppingItemsShoppingListMembersOnly]
+    
+    def get_queryset(self):
+        shopping_list = self.kwargs["pk"]
+        return ShoppingItem.objects.filter(shopping_list=shopping_list)
